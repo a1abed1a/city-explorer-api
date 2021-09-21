b62bed4bf4d50ed222fbfff3092998f0d5569b84
a1abed1a <abedshehadeh1@gmail.com>
2021-09-21 23:27:06 +0300

    done lab 8

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// http://localhost:3050/
+
 // const { request, response} = require('express')
 
 const express = require('express');
@@ -10,6 +12,8 @@ const server = express();
 const cors = require('cors');
 server.use(cors());
 
+const axios = require('axios');
+
 const data = require('./data/weather.json');
 
 const PORT = process.env.PORT;
@@ -17,25 +21,38 @@ const PORT = process.env.PORT;
 server.get('/', (req, res) => {
     res.status(200).send('Hi and hello');
 });
-
-// /weather?locatioName=<locatioName>
-server.get('/weather', (req, res) => {
+// /weather?lat=<lat>&lon=<lon>
+ server.get('/weather', async(req, res) => {
     
-    let locatioName = req.query.locatioName;
+    let lat = req.query.lat;
+    let lon = req.query.lon;
 
-    let locationData = data.find((item)=>{
-        if(item.city_name.toLocaleLowerCase() === locatioName.toLocaleLowerCase()) {
-            return item
-        }
+    let weatherData = await axios.get(`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`);
+
+    let finalData = weatherData.data.data.map(ele => {
+        return{description:`Temperature ${ele.temp}°C with ${ele.weather.description}`, date:ele.datetime}
     })
-    let finalData = locationData.data.map(ele => {
-        return{description:`Low of ${ele.low_temp}, high of ${ele.max_temp} with ${ele.weather.description}`, date:ele.valid_date}
-    })
+
     res.send(finalData)
 });
 
+// /movie?title=<title>
+server.get('/movie', async(req, res) => {
+
+    let title = req.query.title;
+
+    let temMovieData = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1`);
+    let movieData = temMovieData.data.results;
+    for (let i = 1; i < 5; i++) {
+        temMovieData = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=${i+1}`);
+        movieData = movieData.concat(temMovieData.data.results);     
+    }
+    res.send(movieData.filter(ele=> ele.title.toLowerCase().includes(title.toLowerCase())))
+});
+
 server.get('/test',(req,res)=>{
-    res.send('API server is working')
+    let a =[]
+    res.send(`API server is working ${a.length > 0 ? 'yes':'no'}`)
 })
 
 server.get('*',(req,res)=>{
